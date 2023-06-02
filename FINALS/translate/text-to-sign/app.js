ae40001376a22bee3b3f93e4c6d6b9ccee3a0ba7
Francine <112456101+Syntaaax@users.noreply.github.com>
2023-06-02 10:54:30 +0800

    Add files via upload

---
+++ FINALS/translate/text-to-sign/app.js
@@ -0,0 +1,43 @@
+const signText = document.querySelector('.player-view')
+const normText = document.querySelector('#player-text')
+
+const textArea = document.querySelector('#text-area')
+
+const msgInput = document.querySelector('.message-input')
+const sendBtn = document.querySelector('#send-btn')
+
+msgInput.value = 'ihioahsdhashdskd'
+
+let newText = "";
+sendBtn.addEventListener('click', function() {
+    // textArea.value = ''
+    textArea.innerHTML = ''
+    let text = msgInput.value;
+    msgInput.value = ''
+
+
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(function() {
+            signText.innerHTML = text[i]
+            normText.innerHTML = text[i]
+
+
+            const letter = text.charAt(i);
+            const isLastLetter = (i === text.length - 1);
+
+            if (isLastLetter) {
+                // newText += `<p style="background-color:red;">${letter}</p>`;
+                newText += letter
+
+
+            } else {
+                newText += letter;
+            }
+
+            textArea.innerHTML = newText;
+
+
+            console.log(i)
+        }, i * 1000)
+    }
+})
