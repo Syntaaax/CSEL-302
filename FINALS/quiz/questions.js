9d05d0f86f97f076aba4fc625b53e139e5c7bac1
Francine <112456101+Syntaaax@users.noreply.github.com>
2023-06-02 12:02:18 +0800

    Add files via upload

---
+++ FINALS/quiz/questions.js
@@ -0,0 +1,340 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Guess the Sign Language",
+    image: "images/x.png",
+    answer: "X",
+    options: [
+      "T",
+      "P",
+      "X",
+      "B"
+    ]
+  },
+
+  {
+    numb: 2,
+    question: "Guess the Sign Language",
+    image: "images/s.png",
+    answer: "S",
+    options: [
+      "A",
+      "S",
+      "G",
+      "B"
+    ]
+  },
+
+  {
+    numb: 3,
+    question: "Guess the Sign Language",
+    image: "images/z.png",
+    answer: "Z",
+    options: [
+      "U",
+      "Z",
+      "J",
+      "V"
+    ]
+  },
+
+  {
+    numb: 4,
+    question: "Guess the Sign Language",
+    image: "images/f.png",
+    answer: "F",
+    options: [
+      "O",
+      "L",
+      "F",
+      "R"
+    ]
+  },
+
+  {
+    numb: 5,
+    question: "Guess the Sign Language",
+    image: "images/h.png",
+    answer: "H",
+    options: [
+      "I",
+      "P",
+      "H",
+      "Q"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Guess the Sign Language",
+    image: "images/u.png",
+    answer: "U",
+    options: [
+      "U",
+      "K",
+      "G",
+      "L"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Guess the Sign Language",
+    image: "images/i.png",
+    answer: "I",
+    options: [
+      "N",
+      "Z",
+      "W",
+      "I"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Guess the Sign Language",
+    image: "images/s.png",
+    answer: "S",
+    options: [
+      "A",
+      "S",
+      "F",
+      "D"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Guess the Sign Language",
+    image: "images/k.png",
+    answer: "K",
+    options: [
+      "F",
+      "K",
+      "E",
+      "T"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Guess the Sign Language",
+    image: "images/b.png",
+    answer: "B",
+    options: [
+      "B",
+      "N",
+      "M",
+      "R"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "Guess the Sign Language",
+    image: "images/l.png",
+    answer: "L",
+    options: [
+      "A",
+      "Z",
+      "C",
+      "L"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "Guess the Sign Language",
+    image: "images/r.png",
+    answer: "R",
+    options: [
+      "R",
+      "L",
+      "N",
+      "B"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "Guess the Sign Language",
+    image: "images/d.png",
+    answer: "D",
+    options: [
+      "C",
+      "K",
+      "D",
+      "P"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "Guess the Sign Language",
+    image: "images/w.png",
+    answer: "W",
+    options: [
+      "I",
+      "B",
+      "W",
+      "E"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Guess the Sign Language",
+    image: "images/N.png",
+    answer: "N",
+    options: [
+      "N",
+      "H",
+      "S",
+      "X"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "Guess the Sign Language",
+    image: "images/a.png",
+    answer: "A",
+    options: [
+      "T",
+      "E",
+      "A",
+      "V"
+    ]
+  },
+
+  {
+    numb: 17,
+    question: "Guess the Sign Language",
+    image: "images/o.png",
+    answer: "O",
+    options: [
+      "O",
+      "P",
+      "Q",
+      "Y"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "Guess the Sign Language",
+    image: "images/g.png",
+    answer: "G",
+    options: [
+      "G",
+      "Z",
+      "I",
+      "L"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "Guess the Sign Language",
+    image: "images/p.png",
+    answer: "P",
+    options: [
+      "H",
+      "J",
+      "P",
+      "F"
+    ]
+  },
+
+  {
+    numb: 20,
+    question: "Guess the Sign Language",
+    image: "images/v.png",
+    answer: "V",
+    options: [
+      "T",
+      "K",
+      "F",
+      "V"
+    ]
+  },
+
+  {
+    numb: 21,
+    question: "Guess the Sign Language",
+    image: "images/m.png",
+    answer: "M",
+    options: [
+      "U",
+      "O",
+      "M",
+      "A"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "Guess the Sign Language",
+    image: "images/t.png",
+    answer: "T",
+    options: [
+      "Z",
+      "T",
+      "O",
+      "N"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "Guess the Sign Language",
+    image: "images/c.png",
+    answer: "C",
+    options: [
+      "Q",
+      "D",
+      "C",
+      "G"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "Guess the Sign Language",
+    image: "images/y.png",
+    answer: "Y",
+    options: [
+      "Y",
+      "U",
+      "L",
+      "B"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "Guess the Sign Language",
+    image: "images/j.png",
+    answer: "J",
+    options: [
+      "N",
+      "S",
+      "F",
+      "J"
+    ]
+  },
+
+  {
+    numb: 26,
+    question: "Guess the Sign Language",
+    image: "images/q.png",
+    answer: "Q",
+    options: [
+      "K",
+      "C",
+      "A",
+      "Q"
+    ]
+  },
+
+];
